d3bcb8362beaf04b061dc05d0a64aa234567c049
Damian Aruj <daruj@despegar.com>
2015-05-08 01:19:16 -0300

    included middlewares

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 var ctrlr = require('ctrlr');
+var fs = require('fs');
 
+module.exports = function (expressApp, config) {
 
-module.exports = function(expressApp, config) {
+  require('express-reverse')(expressApp);
 
-    require('express-reverse')(expressApp);
+  if (typeof config.endpoints !== 'object') {
+    throw new Error('config.routes must be set to a valid route object map');
+  }
 
-    if (typeof config.endpoints !== 'object') {
-        throw new Error('config.routes must be set to a valid route object map');
+  if (typeof config.controllers !== 'string') {
+    throw new Error('config.controllers must be set to a valid directory path');
+  }
+
+  if (typeof config.middlewares !== 'string') {
+    throw new Error('config.middlewares must be set to a valid directory path');
+  }
+
+  // Obj of middlewares
+  var mw = {};
+
+  // Let's load all the files in models dir
+  fs.readdirSync(config.middlewares).forEach(function (filename) {
+    if (~filename.indexOf('.js')) {
+      var filename = filename.split('.js')[0];
+      mw[filename] = require(config.middlewares + '/' + filename + '.js');
     }
+  });
 
-    if (typeof config.controllers !== 'string') {
-        throw new Error('config.controllers must be set to a valid directory path');
+  var endpoint;
+  var controllers = ctrlr(config.controllers);
+
+  for (endpoint in config.endpoints) {
+    var meta = config.endpoints[endpoint];
+
+    var method = (meta.method || 'get').toLowerCase();
+    var action = controllers(meta.action);
+    var route = meta.path;
+    var routeMiddlewares = meta.middlewares || [];
+    var middlewares = [mw.log];
+
+    if (routeMiddlewares.length) {
+      routeMiddlewares.forEach(function (middleware) {
+        middlewares.push(mw[middleware]);
+      });
     }
-
-    var controllers = ctrlr(config.controllers);
-    for (var endpoint in config.endpoints) {
-        var meta = config.endpoints[endpoint];
-
-        var method = (meta.method || 'get').toLowerCase();
-        var action = controllers(meta.action);
-        var route = meta.path;
-
-        expressApp[method](endpoint, route, action);
-    }
+    expressApp[method](endpoint, route, middlewares, action);
+  }
 };
 
 
